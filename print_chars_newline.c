dd222eeb03b2f5a37d29e301a8faa95f37c7d3ae
xkyota <max1mkasss.mail@gmail.com>
2025-02-26 14:27:02 +0200

    added comments

--- print_chars_newline.c
+++ print_chars_newline.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int main(void){ 
+    // Initialization of variables
     char userInput[99];
     int i = 0; 
 
-    // printf("Enter a word >>> "); 
+    // Reading user input
     scanf("%s", userInput);
 
+    // Repeat until the symbol is 0
     while (userInput[i] != 0)
     {
+        // Print each character on a new line
         printf("%c\n", userInput[i]); 
+
+        // Increment the i variable to move along the line.
         i++; 
     }
 
